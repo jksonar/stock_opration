6b18ff82314b29d9ae837112fb6bcb7c08d32846
jksonar <jksonar1992@gmail.com>
2024-03-20 00:26:43 +0530

    adding pg_changes

--- crudproject/crudproject/settings.py
+++ crudproject/crudproject/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'crudproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'localhost',
+        'NAME': 'stock_analytics',
+        'OPTIONS': {
+            'service': 'my_service',
+        },
     }
 }
 
