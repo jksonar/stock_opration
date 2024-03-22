bc2f9f65b69f212e822f1d4330705d44766ab930
jksonar <jksonar1992@gmail.com>
2024-03-22 22:20:27 +0530

    working code

--- crudproject/crudapp/views.py
+++ crudproject/crudapp/views.py
@@ -22,15 +22,15 @@ def showView(request):
 
 # list out stocks in specific fund
 def fundStockView(request,s_fname):
-    obj = Stocks.objects.filter(fname=s_fname).order_by('sname')
+    obj = Stocks.objects.filter(fname=s_fname).order_by('-holding')
     template_name = 'crudapp/showfundstock.html'
     context = {'obj': obj}
     return render(request, template_name, context)
 
 # list type of fund having
 def fundStockTypeView(request,s_ftype):
-    obj = Stocks.objects.filter(ftype=s_ftype).order_by('ftype')
-    template_name = 'crudapp/showfundstock.html'
+    obj = Stocks.objects.filter(ftype=s_ftype).distinct('fname').order_by()
+    template_name = 'crudapp/showfundname.html'
     context = {'obj': obj}
     return render(request, template_name, context)
 
